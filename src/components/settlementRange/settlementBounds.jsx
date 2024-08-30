6826fef8250163678a7785cf52dc920776756c3c
lailahotprospector <laila@hotprospector.com>
2024-08-30 19:23:42 +0400

    chat update

--- src/components/settlementRange/settlementBounds.jsx
+++ src/components/settlementRange/settlementBounds.jsx
@@ -22,84 +22,40 @@ const StyledTableContainer = styled(TableContainer)({
 });
 
 export default function SettlementBounds({ data }) {
+  // Extract settlement objects from data
+  const settlements = [
+    data?.settlement_range_1,
+    data?.settlement_range_2,
+    data?.settlement_range_3,
+  ];
+
+  // Ensure settlements are defined and extract unique keys from the first non-null settlement
+  const keys = Object.keys(settlements.find((settlement) => settlement) || {});
+
   return (
     <StyledTableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <StyledTableCell></StyledTableCell>
-            <StyledTableCell align="center">Settlement 1</StyledTableCell>
-            <StyledTableCell align="center">Settlement 2</StyledTableCell>
-            <StyledTableCell align="center">Settlement 3</StyledTableCell>
+            <StyledTableCell></StyledTableCell> {/* Empty header for row labels */}
+            {settlements.map((_, index) => (
+              <StyledTableCell key={index} align="center">
+                {`Settlement ${index + 1}`}
+              </StyledTableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow>
-            <StyledTableCell>Lower Bound</StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_1?.weekly_payment_range?.[0] ||
-                data?.settlement_range_1?.lower_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_2?.weekly_payment_range?.[0] ||
-                data?.settlement_range_2?.lower_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_3?.weekly_payment_range?.[0] ||
-                data?.settlement_range_3?.lower_bound}
-            </StyledTableCell>
-          </TableRow>
-          <TableRow>
-            <StyledTableCell>Weeks to Pay off Lower</StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_1?.weeks_to_pay_off_lower_bound ||
-                data?.settlement_range_1?.weeks_to_payoff_lower_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_2?.weeks_to_pay_off_lower_bound ||
-                data?.settlement_range_2?.weeks_to_payoff_lower_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_3?.weeks_to_pay_off_lower_bound ||
-                data?.settlement_range_3?.weeks_to_payoff_lower_bound}
-            </StyledTableCell>
-          </TableRow>
-          <TableRow>
-            <StyledTableCell>Upper Bound</StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_1?.weekly_payment_range?.[1] ||
-                data?.settlement_range_1?.upper_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_2?.weekly_payment_range?.[1] ||
-                data?.settlement_range_2?.upper_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              $
-              {data?.settlement_range_3?.weekly_payment_range?.[1] ||
-                data?.settlement_range_3?.upper_bound}
-            </StyledTableCell>
-          </TableRow>
-          <TableRow>
-            <StyledTableCell>Weeks to Pay off Upper</StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_1?.weeks_to_pay_off_upper_bound ||
-                data?.settlement_range_1?.weeks_to_payoff_upper_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_2?.weeks_to_pay_off_upper_bound ||
-                data?.settlement_range_2?.weeks_to_payoff_upper_bound}
-            </StyledTableCell>
-            <StyledTableCell align="center">
-              {data?.settlement_range_3?.weeks_to_pay_off_upper_bound ||
-                data?.settlement_range_3?.weeks_to_payoff_upper_bound}
-            </StyledTableCell>
-          </TableRow>
+          {keys.map((key) => (
+            <TableRow key={key}>
+              <StyledTableCell>{key.replace(/_/g, " ")}</StyledTableCell>
+              {settlements.map((settlement, index) => (
+                <StyledTableCell key={index} align="center">
+                  {settlement ? settlement[key] : "-"}
+                </StyledTableCell>
+              ))}
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </StyledTableContainer>
