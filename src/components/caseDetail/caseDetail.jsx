f7d593b5935e021c99d2f03370704922999bc5cb
tamoorkhokhar1 <tamoor@hotprospector.com>
2024-09-13 23:09:55 +0500

    add other creditors info

--- src/components/caseDetail/caseDetail.jsx
+++ src/components/caseDetail/caseDetail.jsx
@@ -423,7 +423,7 @@ function CaseDetail() {
                                   fontSize: "11px",
                                 }}
                               >
-                                <Hidden mdDown>
+                                <Hidden smDown>
                                   <span
                                     style={{
                                       fontWeight: "700",
@@ -434,10 +434,13 @@ function CaseDetail() {
                                     Name
                                   </span>
                                 </Hidden>
-                                {item?.name}
+                                {
+                                  item?.creditor?.businessInformation
+                                    ?.companyName
+                                }
                               </span>
                             </Grid>
-                            <Hidden mdDown>
+                            <Hidden smDown>
                               <Grid item xs={3} sm={4} lg={6}>
                                 <span
                                   style={{
