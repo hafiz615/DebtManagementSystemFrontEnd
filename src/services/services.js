2234c304fa5fa3c79ce47b2ad145f533b43f102b
Laila Khan <laila@hotprospector.com>
2024-12-31 21:49:55 +0500

    summary api integeration

--- src/services/services.js
+++ src/services/services.js
@@ -1647,3 +1647,14 @@ export const UpdateManualPayments = async (payload, id) => {
     return error;
   }
 };
+export const CallSummary = async (payload) => {
+  try {
+    return await axios.post(
+      BASE_URL + `/v1/call/callSummary`,
+      payload,
+      setHeaders()
+    );
+  } catch (error) {
+    return error;
+  }
+};
