0933f150e38a6f0143edb7ab1a7cee3ff96b686c
RummazAhmadKhan <rummazkhan@gmail.com>
2025-06-23 12:40:58 +0500

    telnyx incoming feature

--- src/App.js
+++ src/App.js
@@ -32,118 +32,9 @@ import DialPad from "./components/dialPad";
 import VoiceMailPage from "./pages/voicemailPage";
 import PaymentCheckout from "./pages/paymentCheckout";
 import Thankyou from "./pages/thankyou";
+import { TelnyxRTCProvider } from "@telnyx/react-client";
 
 function App() {
-  // const [incomingCall, setIncomingCall] = useState(null);
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-  // const [callDuration, setCallDuration] = useState(0);
-  // const [callInterval, setCallInterval] = useState(null);
-  // const [allCases, setAllCases] = useState({});
-  // const [callSid, setCallSid] = useState();
-  // const [callerName, setCallerName] = useState();
-  // const [caseMenuActive, setCaseMenuActive] = useState(false);
-  // const [token, setToken] = useState(localStorage.getItem("twilioToken"));
-  // const navigate = useNavigate();
-
-  // const getCreditorCompanies = async () => {
-  //   const res = await GetAllUserCases();
-  //   if (res?.status === 200) {
-  //     setAllCases(res?.data?.data);
-  //   }
-  // };
-
-  // const getCallSID = async (callSid) => {
-  //   const SIDres = await GetCallSid(callSid);
-  //   if (SIDres?.status === 200) {
-  //     setCallSid(SIDres?.data?.data);
-  //   }
-  // };
-
-  // const getNameFromCall = async (from) => {
-  //   const payload = {
-  //     from: from?.replace(/^client:\+1/, ""),
-  //   };
-  //   const callerNameRes = await GetCallerName(payload);
-  //   if (callerNameRes?.status === 200) {
-  //     setCallerName(callerNameRes?.data?.data);
-  //     if (callerNameRes?.data?.data?.caseId) {
-  //       localStorage.setItem("route", "all-cases");
-  //       navigate(`/all-cases/${callerNameRes?.data?.data?.caseId}`);
-  //     }
-  //   }
-  // };
-
-  // const initializeDevice = () => {
-  //   const token = localStorage.getItem("twilioToken") || "";
-  //   if (window.twilioDevice) {
-  //     window.twilioDevice.destroy();
-  //   }
-  //   const twilioDevice = new Device(token, {
-  //     logLevel: 1,
-  //     codecPreferences: ["opus", "pcmu"],
-  //   });
-
-  //   twilioDevice.register();
-  //   window.twilioDevice = twilioDevice;
-
-  //   twilioDevice.on("incoming", (incomingCall) => {
-  //     setCaseMenuActive(false);
-  //     setIncomingCall(incomingCall);
-  //     setIsModalOpen(true);
-  //     getCreditorCompanies();
-  //     getNameFromCall(incomingCall?.parameters?.From);
-  //     getCallSID(incomingCall?.parameters?.CallSid);
-
-  //     incomingCall.on("disconnect", () => {
-  //       setIncomingCall(null);
-  //       setCallInterval(null);
-  //       setCallDuration(0);
-  //       setCaseMenuActive(true);
-  //       setCallerName("");
-  //     });
-
-  //     incomingCall.on("cancel", () => {
-  //       setIncomingCall(null);
-  //       setIsModalOpen(false);
-  //       setCallInterval(null);
-  //       setCallDuration(0);
-  //       setCallerName("");
-  //     });
-  //   });
-  //   twilioDevice.on("tokenWillExpire", async () => {
-  //     const twilioTokenResponse = await GetCallToken();
-  //     if (twilioTokenResponse?.status === 200) {
-  //       let twilioToken = twilioTokenResponse.data.data.token;
-  //       localStorage.setItem("twilioToken", twilioToken);
-  //       twilioDevice.updateToken(token);
-  //     }
-  //   });
-  // };
-
-  // useEffect(() => {
-  //   const updateToken = () => {
-  //     const newToken = localStorage.getItem("twilioToken");
-  //     if (newToken !== token) {
-  //       setToken(newToken);
-  //     }
-  //   };
-  //   window.addEventListener("storage", updateToken);
-  //   const interval = setInterval(updateToken, 100);
-  //   return () => {
-  //     window.removeEventListener("storage", updateToken);
-  //     clearInterval(interval);
-  //   };
-  // }, [token]);
-
-  // useEffect(() => {
-  //   if (token) {
-  //     initializeDevice();
-  //   }
-  //   return () => {
-  //     clearInterval(callInterval);
-  //   };
-  // }, [token]);
-
   return (
     <>
       <Routes>
@@ -305,23 +196,16 @@ function App() {
 
         <Route exact path="/set-password" element={<VerifyProfilePage />} />
       </Routes>
-      {/* {location.pathname !== "/" && (
-        <IncomingCall
-          incomingCall={incomingCall}
-          setIncomingCall={setIncomingCall}
-          isModalOpen={isModalOpen}
-          setIsModalOpen={setIsModalOpen}
-          callDuration={callDuration}
-          setCallDuration={setCallDuration}
-          callInterval={callInterval}
-          setCallInterval={setCallInterval}
-          allCases={allCases}
-          callSid={callSid}
-          caseMenuActive={caseMenuActive}
-          setCaseMenuActive={setCaseMenuActive}
-          callerName={callerName}
-        />
-      )} */}
+      {location.pathname !== "/" && (
+        <TelnyxRTCProvider
+          credential={{
+            login: "DMSDev",
+            password: "12345!lumino",
+          }}
+        >
+          <IncomingCall />
+        </TelnyxRTCProvider>
+      )}
       <DialPad />
     </>
   );
